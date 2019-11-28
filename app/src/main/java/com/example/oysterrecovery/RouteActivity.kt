78f8c3ba254114dab0ab10d9f02072706c4120ad
bsallah <bsallah@umd.edu>
2019-11-27 19:36:24 -0500

    broken my routes

--- app/src/main/java/com/example/oysterrecovery/RouteActivity.kt
+++ app/src/main/java/com/example/oysterrecovery/RouteActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -14,16 +15,16 @@ import kotlinx.android.synthetic.main.activity_route.*
 import kotlinx.android.synthetic.main.content_route.*
 
 class RouteActivity : AppCompatActivity() {
-
     val database = FirebaseDatabase.getInstance()
     val mRestaurantRef = database.getReference("restaurants")
     val resList = ArrayList<Restaurant>()
-
+    lateinit var mRecyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_route)
         setSupportActionBar(toolbar)
 
+        var hashMap : HashMap<String, String> = HashMap<String, String> ()
         mRestaurantRef.addValueEventListener(object: ValueEventListener {
             override fun onCancelled(p0: DatabaseError) {
                 Log.w(TAG, "Failed to read restaurant")
@@ -38,7 +39,7 @@ class RouteActivity : AppCompatActivity() {
                     val name = restaurant.name
                     val capacity = restaurant.oysterCapacity
                     val num = restaurant.oysterNumber
-
+                    hashMap.put(name, addy)
                     Log.d(TAG, "RestaurantID is $key")
                     Log.d(TAG, "Resaturant addy is $addy\n Restaurant name is $name\n" +
                             "Restaurant capacity is $capacity\n Restaurant oyster amount is $num ")
@@ -54,8 +55,12 @@ class RouteActivity : AppCompatActivity() {
             val int =  Intent(this, MyRouteActivity::class.java)
             startActivity(int)
         }
+        Log.d(TAG, "$hashMap")
+        setContentView(R.layout.content_my_route)
+        mRecyclerView = findViewById(R.id.route3)
+        RecyclerView_Config().setConfig(mRecyclerView, this@RouteActivity, , hashMap.keys.toList())
     }
-    
+
     companion object {
         val TAG = "FirebaseDB Reading"
     }
